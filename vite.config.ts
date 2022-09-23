e6ca339ddf70bc4f16be4bbdb12de716e3335e7d
Andre Schalkau <andre@catchafish.dev>
2022-09-23 15:15:17 +0200

    removed unused stuff

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Unocss from 'unocss/vite'
 
 export default defineConfig({
   resolve: {
